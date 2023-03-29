e3fe8fbff443ece01d9b9085811d5919fe883b35
sebalp100 <sebalp100@gmail.com>
2023-03-29 16:11:34 -0300

    Add cancancan authorization and delete post method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,27 @@
 class CommentsController < ApplicationController
+  load_and_authorize_resource
   def new
     @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
   def create
+    @user = User.find(params[:user_id])
+    if @user.id != current_user.id
+      flash[:alert] = 'You cannot create comments for another user.'
+      redirect_to root_path and return
+    end
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.author = current_user
-
     if @comment.save
       redirect_to user_posts_path, notice: 'Comment was successfully created.'
     else
       render :new
     end
+  end
+
+  def destroy
   end
 
   private

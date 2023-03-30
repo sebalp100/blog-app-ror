3d53493897ff9c3c914351afe12f8f6d69fc1216
sebalp100 <sebalp100@gmail.com>
2023-03-30 16:05:17 -0300

    Add posts api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,14 @@ Rails.application.routes.draw do
       resources :comments, only: [:create, :new, :destroy, :index]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+        resources :posts, only: [:index]
+      end
+    end
+  end
+  
   root "users#index"
 end
